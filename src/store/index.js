2094ac97d587145e9ec70e71c1c5c34fc3d438f6
JuanEMD <juanernestomarmolejosdanis@gmail.com>
2022-05-26 10:02:27 -0400

    Same but with comments

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import router from '../router/'
+import router from "../router/";
 
 export default createStore({
   state: {
@@ -14,91 +14,99 @@ export default createStore({
   },
   mutations: {
     cargar(state, payload) {
-      state.tareas = payload
+      state.tareas = payload;
     },
     set(state, payload) {
       state.tareas.push(payload);
     },
-    delete(state, payload){
-      state.tareas = state.tareas.filter(item => item.id !== payload);
+    delete(state, payload) {
+      state.tareas = state.tareas.filter((item) => item.id !== payload);
     },
-    edit(state, payload){
-      if(!state.tareas.find(item => item.id === payload)){
-        router.push('/');
+    edit(state, payload) {
+      if (!state.tareas.find((item) => item.id === payload)) {
+        router.push("/");
         return;
       }
-      state.tarea = state.tareas.find(item => item.id === payload);
+      state.tarea = state.tareas.find((item) => item.id === payload);
     },
-    update(state, payload){
-      state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item);
-      router.push('/')
+    update(state, payload) {
+      state.tareas = state.tareas.map((item) =>
+        item.id === payload.id ? payload : item
+      );
+      router.push("/");
     },
-
   },
   actions: {
-    async cargarLocalStorage({commit}){
+    //Leer data de firebase
+    async cargarLocalStorage({ commit }) {
       try {
-        const respuesta = await fetch('https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas.json')
-        const dataDB = await respuesta.json()
-        const arrayTareas = []
+        const respuesta = await fetch(
+          "https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas.json"
+        );
+        const dataDB = await respuesta.json();
+        const arrayTareas = [];
 
-        for (let id in dataDB){
-          arrayTareas.push(dataDB[id])
+        for (let id in dataDB) {
+          arrayTareas.push(dataDB[id]);
         }
-        console.log(arrayTareas)
-        commit('cargar', arrayTareas)
-
+        commit("cargar", arrayTareas);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-
     },
+    //agregar tareas a firebase
     async setTareas({ commit }, tarea) {
       try {
-        const respuesta = await fetch(`https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${tarea.id}.json`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(tarea),
-        })
+        const respuesta = await fetch(
+          `https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${tarea.id}.json`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(tarea),
+          }
+        );
 
-        const dataDB = await respuesta.json()
-
+        const dataDB = await respuesta.json();
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
       commit("set", tarea);
     },
-    async deleteTareas({ commit }, id){
-
+    //borrar tareas de firebase
+    async deleteTareas({ commit }, id) {
       try {
-        await fetch(`https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${id}.json`,{
-          method: 'DELETE'
-        })
-        commit('delete', id);
-
+        await fetch(
+          `https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${id}.json`,
+          {
+            method: "DELETE",
+          }
+        );
+        commit("delete", id);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
+    //editar tareas de firebase
     editTarea({ commit }, id) {
       commit("edit", id);
     },
-    async updateTarea({commit}, tarea){
-
+    async updateTarea({ commit }, tarea) {
       try {
-        const respuesta = await fetch(`https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${tarea.id}.json`, {
-          method: 'PATCH',
-          body: JSON.stringify(tarea)
-        })
-        const dataDB = await respuesta.json()
-        commit('update', dataDB )
+        const respuesta = await fetch(
+          `https://crudvuexfirebase-default-rtdb.firebaseio.com//tareas/${tarea.id}.json`,
+          {
+            method: "PATCH",
+            body: JSON.stringify(tarea),
+          }
+        );
+        const dataDB = await respuesta.json();
+        commit("update", dataDB);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-
-    }
+    },
   },
   modules: {},
 });
